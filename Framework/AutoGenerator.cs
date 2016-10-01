f32cb17bf76ea82281c34b5790599f30a903535b
mkalinic <mkalinic@bitbucket.org/mkalinic/gaas.git>
2016-10-01 11:31:03 +0200

    Add app

---
+++ Framework/AutoGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public class AutoGenerator
+    {
+        public static int GetRandomNumber()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 15);
+        }
+    }
+}
---
+++ LuckyGuess/Program.cs
@@ -0,0 +1,49 @@
+﻿using Framework;
+using System;
+
+namespace LuckyGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = AutoGenerator.GetRandomNumber();
+            int nextNumber;
+            Console.WriteLine($"Guess if next is smaller or bigger S/B?");
+            while (true)
+            {                
+                nextNumber = AutoGenerator.GetRandomNumber();
+                Console.WriteLine($"Number is {number}");
+                while (true)
+                {                       
+                    ConsoleKeyInfo name = Console.ReadKey(true);
+                    if (name.KeyChar == 's' && nextNumber <= number)
+                    {
+                        Console.WriteLine($"You won!");
+                        break;
+                    }
+                    else if (name.KeyChar == 's' && nextNumber > number)
+                    {
+                        Console.WriteLine($"You lose!");
+                        break;
+                    }
+                    else if (name.KeyChar == 'b' && nextNumber >= number)
+                    {
+                        Console.WriteLine($"You won!");
+                        break;
+                    }
+                    else if (name.KeyChar == 'b' && nextNumber < number)
+                    {
+                        Console.WriteLine($"You lose!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Try again");
+                    }
+                }
+                number = nextNumber;
+            }
+        }
+    }
+}
