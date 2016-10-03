a04804effacf82aa5425c0a2e5e30cd4c27d999d
mkalinic <mkalinic@bitbucket.org/mkalinic/gaas.git>
2016-10-03 21:18:00 +0200

    Unit tests

---
+++ FrameworkTest/AutoGeneratorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Framework;
+
+namespace FrameworkTest
+{
+    [TestClass]
+    public class AutoGeneratorTest
+    {
+        [TestMethod]
+        public void TestGetRandomNumberType()
+        {
+            var nextNumber = AutoGenerator.GetRandomNumber();
+            Assert.IsInstanceOfType(nextNumber, typeof(int));            
+        }
+
+        [TestMethod]
+        public void TestGetRandomNumberNotZero()
+        {
+            var nextNumber = AutoGenerator.GetRandomNumber();
+            Assert.AreNotEqual(nextNumber, 0);
+        }
+
+        [TestMethod]
+        public void TestGetRandomNumberSmallerThen15()
+        {
+            var nextNumber = AutoGenerator.GetRandomNumber();
+            Assert.IsTrue(nextNumber < 15);
+        }
+    }
+}
